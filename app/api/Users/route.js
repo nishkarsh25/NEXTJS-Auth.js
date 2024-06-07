1e909796edf25edd58c4ba2e6b9387b10a662a4e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:40:00 +0530

    Handling the Request:
    const body = await req.json();: Parses the incoming request body as JSON.
    const userData = body.formData;: Extracts the formData property from the request body, which contains the user data to be processed.

--- app/api/Users/route.js
+++ app/api/Users/route.js
@@ -3,5 +3,20 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
 export async function POST(req) {
-  
+  try {
+    const body = await req.json();
+    const userData = body.formData;
+
+    
+
+    
+    
+
+    
+
+    
+    
+  } catch (error) {
+    
+  }
 }
