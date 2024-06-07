9496473a2fc0b22f055d4ca4deadadf1d640630f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:36:21 +0530

    useSession Hook:
    const { data: session } = useSession({ ... });: This line calls the useSession hook to retrieve the session data. The useSession hook returns an object that includes session data (session), a loading state (loading), and an error state (error).

--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -4,7 +4,12 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 const Member = async () => {
-  
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/ClientMember");
+    },
+  });
 
   
 };
