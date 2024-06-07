15441af8e7ad632568d9f4d9c688e5011c70c08f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:44:37 +0530

    JSX Structure:
    The component returns a form wrapped in a fragment (<>):
    The form includes inputs for the user's name, email, and password. Each input has an onChange handler to update the state and required attribute to enforce validation.
    An input of type submit is provided to submit the form.
    An error message is conditionally displayed if errorMessage is set.
    Form Styling:
    
    The form and its elements are styled using Tailwind CSS classes:
    The form has a class to make it flexible, with column layout and a gap between elements.
    Input fields have background color and rounded corners.
    The submit button changes color on hover.
    The error message is styled to appear in red.

--- app/(components)/UserForm.jsx
+++ app/(components)/UserForm.jsx
@@ -35,7 +35,53 @@ const UserForm = () => {
     }
   };
 
-  
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        method="post"
+        className="flex flex-col gap-3 w-1/2"
+      >
+        <h1>Create New User</h1>
+        <label>Full Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.name}
+          className="m-2 bg-slate-400 rounded"
+        />
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.email}
+          className="m-2 bg-slate-400 rounded"
+        />
+        <label>Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+          required={true}
+          value={formData.password}
+          className="m-2 bg-slate-400 rounded"
+        />
+        <input
+          type="submit"
+          value="Create User"
+          className="bg-blue-300 hover:bg-blue-100"
+        />
+      </form>
+      <p className="text-red-500">{errorMessage}</p>
+    </>
+  );
 };
 
 
