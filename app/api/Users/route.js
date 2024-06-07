31b6f016874952f3faf07f8f1ebb214dea2faa8c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:40:13 +0530

    Validation:
    The code checks if email and password fields are present in userData. If either is missing, it returns a response with a status code of 400 and an error message indicating that all fields are required.

--- app/api/Users/route.js
+++ app/api/Users/route.js
@@ -7,7 +7,13 @@ export async function POST(req) {
     const body = await req.json();
     const userData = body.formData;
 
-    
+    //Confirm data exists
+    if (!userData?.email || !userData.password) {
+      return NextResponse.json(
+        { message: "All fields are required." },
+        { status: 400 }
+      );
+    }
 
     
     
