b83fb433791ff4cade8695d4170363d3b9d1ffb5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:18:37 +0530

    Schema Definition:
    const userSchema = new Schema({ ... }, { timestamps: true });: This line defines a schema for the User model. A schema is a structure that defines the shape and content of documents within a MongoDB collection. The schema includes three fields:
    name: A string field to store the user's name.
    email: A string field to store the user's email address.
    password: A string field to store the user's hashed password.
    The schema also has an option { timestamps: true } which tells Mongoose to automatically manage createdAt and updatedAt fields for each document in the collection.

--- app/(models)/User.js
+++ app/(models)/User.js
@@ -4,8 +4,14 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 const userSchema = new Schema(
-  
-  
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
 
