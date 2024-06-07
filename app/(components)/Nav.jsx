827141b82375850cc4f6f2740b111300f24fabac
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:51:13 +0530

    Nav Component Definition:
    The Nav component is defined as an asynchronous arrow function, indicating that it's a Next.js API route handler.

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
 
-
+const Nav = async () => {
+  
+  
+};
 
 
