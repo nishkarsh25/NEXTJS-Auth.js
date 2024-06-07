a6bd1e34f7e81a00e6f6a80396c74a0fbe94de4a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:26:29 +0530

    CredentialsProvider: Configures custom credentials authentication. Defines the name and the credentials object, specifying the fields required for authentication (email and password). The authorize function handles the authentication logic: Attempts to find a user by email in the database. If a user is found, it compares the provided password with the stored hashed password using bcrypt. If the passwords match, it deletes the password field from the user object, assigns a role of "Unverified Email," and returns the user object. If no user is found or passwords do not match, it returns null.

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -41,7 +41,47 @@ export const options = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_Secret,
     }),
-    
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email:",
+          type: "text",
+          placeholder: "your-email",
+        },
+        password: {
+          label: "password:",
+          type: "password",
+          placeholder: "your-password",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const foundUser = await User.findOne({ email: credentials.email })
+            .lean()
+            .exec();
+
+          if (foundUser) {
+            console.log("User Exists");
+            const match = await bcrypt.compare(
+              credentials.password,
+              foundUser.password
+            );
+
+            if (match) {
+              console.log("Good Pass");
+              delete foundUser.password;
+
+              foundUser["role"] = "Unverified Email";
+              return foundUser;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+        return null;
+      },
+    }),
   ],
   
 };
