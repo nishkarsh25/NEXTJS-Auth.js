9c4e6790e95089c2aae050d8d34c3175cac8f9df
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 13:48:39 +0530

    Member Component Definition:
    Member is defined as an asynchronous arrow function, indicating that it's a Next.js API route handler.

--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -2,6 +2,12 @@ import { getServerSession } from "next-auth";
 import { options } from "../api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 
+const Member = async () => {
+  
 
+  
+
+  
+};
 
 
